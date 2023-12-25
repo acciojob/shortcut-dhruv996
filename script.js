888daa65474969e90c0ee34c0f3abf6d14765175
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-25 14:47:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function shortcut(s1, s2) {
-  // your code here
+document.addEventListener('DOMContentLoaded', function () {
+  // Example usage
+  const result1 = shortcut("Amnesty", "International");
+  document.getElementById('result').innerText = `Shortcut of "Amnesty" and "International": '${result1}'`;
+
+  const result2 = shortcut("Hello", "world");
+  document.getElementById('result').innerText += `\nShortcut of "Hello" and "world": '${result2}'`;
+
+  const result3 = shortcut("", "International");
+  document.getElementById('result').innerText += `\nShortcut of empty string and "International": '${result3}'`;
+
+  const result4 = shortcut("Amnesty", "");
+  document.getElementById('result').innerText += `\nShortcut of "Amnesty" and empty string: '${result4}'`;
+});
+
+function shortcut(str1, str2) {
+  const initialLetter1 = str1.charAt(0).toUpperCase();
+  const initialLetter2 = str2.charAt(0).toLowerCase();
+
+  return initialLetter1 + initialLetter2;
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
